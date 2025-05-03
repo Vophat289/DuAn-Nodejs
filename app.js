015f1bf5c46c1abfb72d20e5bcb9b34b3e0b367e
VoPhat <vohongphat2892005@gmail.com>
2025-05-04 03:24:41 +0700

    '

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+const express = require ('express');
+
+const app = express ();
+const PORT = 8080;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!") 
+})
+
+app.get("/subin", (req, res) => {
+    res.send("Hello Bin!") 
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy trên port: ${PORT}`)
+})
